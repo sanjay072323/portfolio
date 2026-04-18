016daf1199d932b0489cb49eb3577f707b1ea0f9
sanjay072323~ <sanjay072323@gmail.com~>
2026-04-19 02:02:34 +0530

    issue for deployment

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eXV3cFQOUuiYKnJl/QSb3BjMi0jXHI+xS47qkfd6JPM=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
+  trailingSlash: true,
   images: {
     unoptimized: true
   }
