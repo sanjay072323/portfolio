543cf76360abc429cd63685219b0adb03d408788
sanjay072323~ <sanjay072323@gmail.com~>
2026-04-19 02:05:49 +0530

    issue for deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio',
-  trailingSlash: true,
   images: {
     unoptimized: true
   }
